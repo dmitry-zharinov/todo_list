7b5ecbda7ecbe847f8a9dae92db5915dcc462176
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-08-12 15:06:21 +0300

    Remove username field from task update view

--- base/views.py
+++ base/views.py
@@ -76,7 +76,7 @@ class TaskCreate(LoginRequiredMixin, CreateView):
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 
